785fa2ccac05dcb65b1e325f915a2e6dde6a99c4
Mati Joseph <crucialniccur@gmail.com>
2025-05-25 10:42:16 +0300

    seeding and faker

---
+++ lib/migrations/versions/4b4b1b909ecc_learn_seeding_data_and_faker.py
@@ -0,0 +1,24 @@
+"""learn seeding data and faker
+
+Revision ID: 4b4b1b909ecc
+Revises: 3cf5ba457650
+Create Date: 2025-05-25 10:41:50.324594
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4b4b1b909ecc'
+down_revision = '3cf5ba457650'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
